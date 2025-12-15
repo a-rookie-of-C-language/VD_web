d1847a197957567646410ca9f7952b389ce5661d
不会C的菜鸟 <2128194521@qq.com>
2025-12-16 00:07:50 +0800

    更新安卓network问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,6 +12,7 @@ const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         component: MainLayout,
+        meta: { requiresAuth: true },
         children: [
             {
                 path: '',
